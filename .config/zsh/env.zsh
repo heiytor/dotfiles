cfe40f3553c6af867a1f83625a6c545117af2699
Heitor Danilo <heitor.danilo@icloud.com>
2025-11-02 19:06:29 -0300

    add ssh-agent on non-ssh sessions

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -1,2 +1,6 @@
 export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 export PATH="/home/heitor/.cache/.bun/bin:$PATH"
+
+if [[ -z $SSH_CONNECTION && -z $SSH_AUTH_SOCK ]]; then
+    eval "$(ssh-agent -s)" > /dev/null 2>&1
+fi
