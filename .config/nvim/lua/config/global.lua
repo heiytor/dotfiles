956de91d4fb04c41c2665f69855bab428b62f076
Heitor Danilo <heitor.danilo@icloud.com>
2026-08-11 18:48:56 -0300

    Drop unused Inlay_hints_mode global

--- .config/nvim/lua/config/global.lua
+++ .config/nvim/lua/config/global.lua
@@ -1,5 +1,4 @@
 Inlay_hints_status = true -- Define whether to start using inlay hints or not.
-Inlay_hints_mode = "i" -- Define in which mode the inlay hint should be enabled
 Inlay_hints_filetypes = { -- Define which filetypes will enable inlay hints
 	"go",
 	"ruby",
