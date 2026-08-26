daba58a5cb7678051b7f53edf812266075d985e8
Heitor Danilo <heitor.danilo@icloud.com>
2026-08-26 08:51:39 -0300

    Answer the hyprpm trust prompt and stop it from consuming the plugin list

--- .de-tools/modules/hypr.sh
+++ .de-tools/modules/hypr.sh
@@ -10,17 +10,17 @@ sync_hypr_plugins() {
 
     local failed=0
 
-    if ! hyprpm update; then
+    if ! hyprpm update <<< "Y"; then
         return 2
     fi
 
-    while read -r plugin_name plugin_url; do
+    while read -r plugin_name plugin_url <&3; do
         if [[ -z "$plugin_name" || "$plugin_name" == \#* ]]; then
             continue
         fi
 
         if ! hyprpm list 2>/dev/null | grep -q "Plugin $plugin_name"; then
-            if ! hyprpm add "$plugin_url"; then
+            if ! hyprpm add "$plugin_url" <<< "Y"; then
                 failed=1
                 continue
             fi
@@ -29,7 +29,7 @@ sync_hypr_plugins() {
         if ! hyprpm enable "$plugin_name"; then
             failed=1
         fi
-    done < "$plugins_file"
+    done 3< "$plugins_file"
 
     if (( failed )); then
         return 2
